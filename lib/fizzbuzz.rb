7d2690f2c5148e061ea399517acb60d8f195fea6
Adam Snow <adamjohnsnow@icloud.com>
2017-04-05 16:50:47 +0100

    Completed

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,18 @@
+def fizzbuzz(n)
+
+#in one line, for fun
+ (n % 3 == 0 && n % 5 == 0) ? 'fizzbuzz': ((n % 3 == 0)? "fizz" : ((n % 5 == 0)? "buzz": n))
+
+  # if n % 3 == 0 && n % 5 == 0
+  #   'fizzbuzz'
+  # elsif
+  #   n % 5 == 0
+  #   'buzz'
+  #   elsif
+  #     n % 3 == 0
+  #     'fizz'
+  #   # else
+  #   #   n
+  # end
+
+end
